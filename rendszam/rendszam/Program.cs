f96a2ca6768ec886052c838383a14a24ca820db3
kcscs <kcscs10@gmail.com>
2019-02-19 20:20:41 +0100

    asd

---
+++ rendszam/rendszam/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rendszam
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+
+            asd
+        }
+    }
+}
